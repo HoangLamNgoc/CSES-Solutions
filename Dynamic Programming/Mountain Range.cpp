9036fb99f624d87e2902c831a8d20479a724c749
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-26 22:58:35 +0700

    Update Mountain Range.cpp

--- Dynamic Programming/Mountain Range.cpp
+++ Dynamic Programming/Mountain Range.cpp
@@ -1,41 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
 
 using namespace std;
-typedef pair<int,int> pii;
-const int maxN = 2e5+1;
-const int INF = 0x3f3f3f3f;
 
-int N, dp[maxN];
+const int MAXN = 2e5 + 5;
+int dp[MAXN];
 vector<int> tallers;
-vector<pii> mountains;
-set<int,greater<int>> S_l;
-set<int> S_r;
+vector<pair<int,int>> mounts;
+set<int, greater<int>> sl;
+set<int, greater<int>> sr;
 
-int main(){
-    scanf("%d", &N);
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-    for(int i = 1, h; i <= N; i++){
-        scanf("%d", &h);
-        mountains.push_back({h, i});
+    int num;
+    cin >> num;
+
+    for(int i = 1, u; i <= num; ++i) {
+        cin >> u;
+        mounts.push_back({u, i});
     }
-    sort(mountains.begin(), mountains.end(), greater<pii>());
 
-    int last_height = INF;
-    for(pii m : mountains){
-        int h = m.first, i = m.second;
+    sort(mounts.begin(), mounts.end(), greater<pair<int,int>>());
 
-        if(last_height != h){
-            for(int t : tallers){
-                S_l.insert(t);
-                S_r.insert(t);
+    int last_height = -1;
+    for(auto m : mounts) {
+        int h = m.first;
+        int i = m.second;
+
+        if(last_height != h) {
+            for(int t : tallers) {
+                sl.insert(t);
+                sr.insert(t);
             }
             tallers.clear();
         }
 
-        auto l_ptr = S_l.lower_bound(i);
-        auto r_ptr = S_r.lower_bound(i);
-        int l = (l_ptr == S_l.end() ? 0 : *l_ptr);
-        int r = (r_ptr == S_r.end() ? 0 : *r_ptr);
+        auto l_ptr = sl.lower_bound(i);
+        auto r_ptr = sr.lower_bound(i);
+
+        int l = (l_ptr == sl.end() ? 0 : *l_ptr);
+        int r = (r_ptr == sr.end() ? 0 : *r_ptr);
+
         dp[i] = max(dp[l], dp[r]) + 1;
 
         tallers.push_back(i);
@@ -43,7 +52,8 @@ int main(){
     }
 
     int best = 0;
-    for(int i = 1; i <= N; i++)
+    for(int i = 1; i <= num; ++i)
         best = max(best, dp[i]);
-    printf("%d\n", best);
+
+    cout << best << '\n';
 }
